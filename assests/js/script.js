1ba4b0c8f728a57ecf6dc4d2337a7973e0d9e6e9
Gourabdeb1619 <gourabdeb1619@gmail.com>
2023-07-31 23:29:46 +0530

    changessaved

--- assests/js/script.js
+++ assests/js/script.js
@@ -53,7 +53,7 @@ function(){
 
 // <!-- typed js effect starts -->
     var typed = new Typed(".typing-text", {
-        strings: ["UX Designing","UI Designing","Product Designing"],
+        strings: ["UX Designing","Wordpress Development","UI Designing"],
         loop: true,
         typeSpeed: 50,
 		backSpeed: 25,
